6f21a3e1427f8a5a11108180c28b318f77f4c932
Thomas <thomas.i.sloane@gmail.com>
2023-02-22 14:21:07 +0000

    changes made

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import collections
 import datetime
-import fluidsynth
 import glob
 import numpy as np
 import pathlib
@@ -8,7 +7,6 @@ import pandas as pd
 import pretty_midi
 import seaborn as sns
 import tensorflow as tf
-import termplotlib as tpl
 #import plotext as plt
 
 from os.path import exists
@@ -87,39 +85,6 @@ raw_notes.head()
 get_note_names = np.vectorize(pretty_midi.note_number_to_name)
 sample_note_names = get_note_names(raw_notes['pitch'])
 sample_note_names[:10]
-
-def plot_piano_roll(notes: pd.DataFrame, count: Optional[int] = None):
-  if count:
-    title = f'First {count} notes'
-  else:
-    title = f'Whole track'
-    count = len(notes['pitch'])
-  plt.figure(figsize=(20, 4))
-  plot_pitch = np.stack([notes['pitch'], notes['pitch']], axis=0)
-  plot_start_stop = np.stack([notes['start'], notes['end']], axis=0)
-  plt.plot(
-      plot_start_stop[:, :count], plot_pitch[:, :count], color="b", marker=".")
-  plt.xlabel('Time [s]')
-  plt.ylabel('Pitch')
-  _ = plt.title(title)
-
-plot_piano_roll(raw_notes, count=100)
-plot_piano_roll(raw_notes)
-
-def plot_distributions(notes: pd.DataFrame, drop_percentile=2.5):
-  plt.figure(figsize=[15, 5])
-  plt.subplot(1, 3, 1)
-  sns.histplot(notes, x="pitch", bins=20)
-
-  plt.subplot(1, 3, 2)
-  max_step = np.percentile(notes['step'], 100 - drop_percentile)
-  sns.histplot(notes, x="step", bins=np.linspace(0, max_step, 21))
-
-  plt.subplot(1, 3, 3)
-  max_duration = np.percentile(notes['duration'], 100 - drop_percentile)
-  sns.histplot(notes, x="duration", bins=np.linspace(0, max_duration, 21))
-
-plot_distributions(raw_notes)
 
 def notes_to_midi(notes: pd.DataFrame, out_file: str, instrument_name: str,velocity: int = 100,) -> pretty_midi.PrettyMIDI:
   pm = pretty_midi.PrettyMIDI()
