c15ce7d2f050e7af6015b41d211d1af86a9e776d
Thomas Sloane <thomas.i.sloane@gmail.com>
2023-03-01 02:44:46 +0000

    changes made to website

--- website/midiRecorder.js
+++ website/midiRecorder.js
@@ -1,22 +1,47 @@
-var notes = document.querySelector(".notes-played"); 
+const slider = document.querySelector(".slider");
+const sliderValue = document.querySelector(".value");
 
-WebMidi.enable(function () {
-  //console.log(WebMidi.inputs);
-  //console.log(WebMidi.outputs);
+slider.addEventListener("input", () => {
+  sliderValue.innerHTML = slider.value;
+});
 
-  //console.log(WebMidi);
 
-  var input = WebMidi.inputs[0];
-  console.log(input);
+const generate = document.querySelector(".generate");
+const undo = document.querySelector(".undo");
+const clear = document.querySelector(".clear");
 
-  if(input)
-  {
-    input.addListener('noteon', 'all',
-        function (e) {
-            console.log("(" + e.note.name + e.note.octave + ")");
-            notes.innerHTML += "(" + e.note.name + e.note.octave + ")";
-        }
-    );
-  }
-  
+generate.addEventListener("click", () => {
+  console.log("generating notes");
+});
+
+undo.addEventListener("click", () => {
+  console.log("undid note");
+})
+
+clear.addEventListener("click", () => {
+  console.log("cleared notes")
+});
+
+const modal = document.querySelector(".modal");
+const modalContainer = document.querySelector(".modal-container");
+const info = document.querySelector(".info");
+const blurCover = document.querySelector(".blur");
+
+info.addEventListener("click", () => {
+    modalContainer.style.zIndex = 2;
+    modalContainer.style.opacity = 1;
+    modal.style.opacity = 1;
+
+    blurCover.style.opacity = 0.6;
+    blurCover.style.zIndex = 1;
+
+});
+
+modalContainer.addEventListener("click", () => {
+  modalContainer.style.zIndex = -1;
+  modalContainer.style.opacity = 0;
+  modal.style.opacity = 0;
+
+  blurCover.style.opacity = 0;
+  blurCover.style.zIndex = -1;
 });
