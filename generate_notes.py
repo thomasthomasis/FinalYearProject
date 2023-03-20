39f59ea3b1fb2cbe6905aa6573ca0f5fd7c4e196
Thomas Sloane <thomas.i.sloane@gmail.com>
2023-03-20 04:49:14 +0000

    changes

---
+++ generate_notes.py
@@ -0,0 +1,176 @@
+import collections
+import glob
+import numpy as np
+import pathlib
+import pandas as pd
+import pretty_midi
+from tensorflow import random
+from tensorflow import data
+from tensorflow import Tensor
+from tensorflow import maximum
+from tensorflow import reduce_mean
+from tensorflow import keras
+from tensorflow import squeeze
+from tensorflow import expand_dims
+from tensorflow import train
+import os
+from midiutil import MIDIFile
+
+seed = 42
+random.set_seed(seed)
+np.random.seed(seed)
+
+#loading model with up to date weights
+seq_length = 1
+vocab_size = 128
+
+input_shape = (seq_length, 3)
+learning_rate = 0.005
+
+inputs = keras.Input(input_shape)
+x = keras.layers.LSTM(128)(inputs)
+
+outputs = {
+  'pitch': keras.layers.Dense(128, name='pitch')(x),
+  'step': keras.layers.Dense(1, name='step')(x),
+  'duration': keras.layers.Dense(1, name='duration')(x),
+}
+
+model = keras.Model(inputs, outputs)
+
+def mse_with_positive_pressure(y_true: Tensor, y_pred: Tensor):
+  mse = (y_true - y_pred) ** 2
+  positive_pressure = 10 * maximum(-y_pred, 0.0)
+  return reduce_mean(mse + positive_pressure)
+
+loss = {
+      'pitch': keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+      'step': mse_with_positive_pressure,
+      'duration': mse_with_positive_pressure,
+}
+
+optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+
+checkpoint_path = "training/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+latest = train.latest_checkpoint(checkpoint_dir)
+
+model.compile(loss=loss, optimizer=optimizer)
+model.load_weights(latest)
+model.summary()
+
+#----------------------------------------
+
+def predict_next_note(notes: np.ndarray, keras_model: keras.Model, temperature: float = 1.0) -> int:
+
+  assert temperature > 0
+
+  # Add batch dimension
+  inputs = expand_dims(notes, 0)
+
+  predictions = model.predict(inputs)
+  pitch_logits = predictions['pitch']
+  step = predictions['step']
+  duration = predictions['duration']
+
+  pitch_logits /= temperature
+  pitch = random.categorical(pitch_logits, num_samples=1)
+  pitch = squeeze(pitch, axis=-1)
+  duration = squeeze(duration, axis=-1)
+  step = squeeze(step, axis=-1)
+
+  # `step` and `duration` values should be non-negative
+  step = maximum(0, step)
+  duration = maximum(0, duration)
+
+  return int(pitch), float(step), float(duration)
+
+def midi_to_notes(midi_file: str) -> pd.DataFrame:
+  pm = pretty_midi.PrettyMIDI(midi_file)
+  instrument = pm.instruments[0]
+  notes = collections.defaultdict(list)
+
+  # Sort the notes by start time
+  sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
+  prev_start = sorted_notes[0].start
+
+  for note in sorted_notes:
+    start = note.start
+    end = note.end
+    notes['pitch'].append(note.pitch)
+    notes['start'].append(start)
+    notes['end'].append(end)
+    notes['step'].append(start - prev_start)
+    notes['duration'].append(end - start)
+    prev_start = start
+
+  return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+
+
+
+def create_array_of_generated_notes(jsonData, numNotes):
+
+  pitch = jsonData['pitch']
+  time = jsonData['time']
+
+  print(pitch)
+  print(time)
+
+  midi_file = MIDIFile(1)
+  track = 0
+  timeAmount = 0
+
+  midi_file.addTempo(track, timeAmount, 120)
+
+  channel = 0
+  volume = 100
+  duration = 1
+  
+  for i in range(len(time)):
+    print(int(pitch[i]))
+    midi_file.addNote(track, channel, (int(pitch[i])), int(time[i]), duration, volume)
+
+
+  with open("output.mid", 'wb') as outf:
+    midi_file.writeFile(outf)
+
+  data_dir = pathlib.Path('')
+
+  filenames = glob.glob(str(data_dir/'*.mid*'))
+
+  notes_file = filenames[0]
+  print(notes_file)
+
+  raw_notes = midi_to_notes(notes_file)
+  print(raw_notes)
+
+  key_order = ['pitch', 'step', 'duration']
+
+  sample_notes = np.stack([raw_notes[key] for key in key_order], axis=1)
+
+  # The initial sequence of notes; pitch is normalized similar to training
+  # sequences
+  input_notes = (sample_notes[:seq_length] / np.array([vocab_size, 1, 1]))
+
+  generated_notes = []
+  prev_start = 0
+
+  temperature = 2.0
+
+  for _ in range(int(numNotes)):
+    pitch, step, duration = predict_next_note(input_notes, model, temperature)
+    start = prev_start + step
+    end = start + duration
+    input_note = (pitch, step, duration)
+    generated_notes.append((*input_note, start, end))
+    input_notes = np.delete(input_notes, 0, axis=0)
+    input_notes = np.append(input_notes, np.expand_dims(input_note, 0), axis=0)
+    prev_start = start
+
+  print(pd.DataFrame(generated_notes, columns=(*key_order, 'start', 'end')))
+
+  return generated_notes
+
+#create_array_of_generated_notes({"pitch":"[1,2,3,4,5]", "time":"[1,2,3,4,5]"}, 10)
+
---
+++ static/midiRecorder.js
@@ -0,0 +1,394 @@
+const slider = document.querySelector(".slider");
+const sliderValue = document.querySelector(".value");
+
+slider.addEventListener("input", () => {
+  sliderValue.innerHTML = slider.value;
+});
+
+var pianoRoll = document.querySelector(".piano-roll");
+var generateButton = document.querySelector(".generate");
+var clearButton = document.querySelector(".clear");
+var playButton = document.querySelector(".play");
+
+var container = document.querySelector(".container-piano-roll");
+var keyboardVisual = document.querySelector(".keyboard-visual");
+
+container.scrollTo({
+  top: "1000",
+  left: 0,
+  behavior: "smooth"
+})
+
+container.addEventListener("scroll", () => {
+  keyboardVisual.scrollTo({
+    top: container.scrollTop,
+    left: "0",
+    behaviour: "smooth"
+  })
+})
+
+generateButton.addEventListener("click", () => {
+  generate();
+});
+
+clearButton.addEventListener("click", () => {
+  clear();
+});
+
+playButton.addEventListener("click", () => {
+  var clickedNotes = document.querySelectorAll('[data-is-clicked="true"]');
+
+  if(playing)
+  {
+    window.clearTimeout(playTimeout);
+
+    for(var i = 0; i < litUpColumn.length; i++)
+    {
+      litUpColumn[i].setAttribute("data-is-playing", "false");
+    }
+
+    iteration = 0;
+    litUpColumn = [];
+
+    pianoRoll.style.pointerEvents = "auto";
+    clearButton.style.pointerEvents = "auto";
+    generateButton.style.pointerEvents = "auto";
+
+    playing = false;
+
+  }
+  else
+  {
+    if(clickedNotes.length != 0)
+    {
+      play();
+
+      pianoRoll.style.pointerEvents = "none";
+      clearButton.style.pointerEvents = "none";
+      generateButton.style.pointerEvents = "none";
+    }
+  }
+
+});
+
+const modal = document.querySelector(".modal");
+const modalContainer = document.querySelector(".modal-container");
+const info = document.querySelector(".info");
+const blurCover = document.querySelector(".blur");
+
+info.addEventListener("click", () => {
+  modalContainer.style.zIndex = 2;
+  modalContainer.style.opacity = 1;
+  modal.style.opacity = 1;
+
+  blurCover.style.opacity = 0.6;
+  blurCover.style.zIndex = 1;
+});
+
+modalContainer.addEventListener("click", () => {
+  modalContainer.style.zIndex = -1;
+  modalContainer.style.opacity = 0;
+  modal.style.opacity = 0;
+
+  blurCover.style.opacity = 0;
+  blurCover.style.zIndex = -1;
+});
+
+
+var columns = document.querySelectorAll(".column");
+var notesClicked = [];
+
+var notes = [
+"g9", "ff9", "f9", "e9", "dd9", "d9", "cc9", "c9",
+"b8", "aa8", "a8", "gg8", "g8", "ff8", "f8", "e8", "dd8", "d8", "cc8", "c8",
+"b7", "aa7", "a7", "gg7", "g7", "ff7", "f7", "e7", "dd7", "d7", "cc7", "c7",
+"b6", "aa6", "a6", "gg6", "g6", "ff6", "f6", "e6", "dd6", "d6", "cc6", "c6",
+"b5", "aa5", "a5", "gg5", "g5", "ff5", "f5", "e5", "dd5", "d5", "cc5", "c5",
+"b4", "aa4", "a4", "gg4", "g4", "ff4", "f4", "e4", "dd4", "d4", "cc4", "c4",
+"b3", "aa3", "a3", "gg3", "g3", "ff3", "f3", "e3", "dd3", "d3", "cc3", "c3",
+"b2", "aa2", "a2", "gg2", "g2", "ff2", "f2", "e2", "dd2", "d2", "cc2", "c2",
+"b1", "aa1", "a1", "gg1", "g1", "ff1", "f1", "e1", "dd1", "d1", "cc1", "c1",]
+
+var noteSounds = notes.reverse();
+
+
+
+var notesActive = 0;
+
+const noteClicked = e => {
+  console.log("Column Clicked: ", e.target.id);
+
+  var columnAmount = document.querySelectorAll(".row")[0].children[document.querySelectorAll(".row")[0].children.length - 1].id;
+  console.log("Column Amount: ", columnAmount);
+
+  if(e.target.getAttribute("data-is-clicked") == "false")
+  {
+    addColumns(e.target.id);
+
+    e.target.setAttribute("data-is-clicked", "true");
+    notesClicked.push(e.target);
+
+    notesActive++;
+
+    var row = (parseInt(e.target.getAttribute("data-row")));
+
+    const audioObject = document.getElementById(noteSounds[row - 24]);
+    const copy = audioObject.cloneNode(true);
+    copy.classList.add("duplicate");
+    
+    var audioDiv = document.querySelector(".audio-files");
+    audioDiv.appendChild(copy);
+    copy.play();
+    window.setInterval(() => {
+      copy.remove();
+    }, 3000)
+  }
+  else
+  {
+    e.target.setAttribute("data-is-clicked", "false");
+    e.target.setAttribute("data-is-generated", "false");
+    notesClicked.push(e.target);
+
+    notesActive--;
+  }
+
+}
+
+for(let column of columns)
+{
+  column.addEventListener("click", noteClicked)
+}
+
+function clear()
+{
+  if(notesActive == 0)
+  {
+    return;
+  }
+
+  for(var i = 0; i < notesClicked.length; i++)
+  {
+    notesClicked[i].setAttribute("data-is-clicked", "false");
+    notesClicked[i].setAttribute("data-is-generated", "false");
+  }
+}
+
+function generate()
+{
+  const clickedNotes = document.querySelectorAll('[data-is-clicked="true"]');
+  var notesColumn = [];
+  var notesRow = [];
+  for(var i = 0; i < clickedNotes.length; i++)
+  {
+    notesColumn.push(parseInt(clickedNotes[i].id));
+    notesRow.push(parseInt(clickedNotes[i].getAttribute("data-row")));
+  }
+
+  notesRow = notesRow.reverse();
+  notesColumn.sort(function(a, b){return a - b});
+
+  console.log(notesColumn);
+  console.log(notesRow);
+
+  var numNotes = document.getElementById("myRange").value;
+
+  var data = {
+    "pitch": notesRow,
+    "time": notesColumn,
+    "numNotes": numNotes 
+  };
+
+  var returnedData;
+
+  $.ajax({
+    type: "POST",
+    url: "/generatenotes",
+    data: JSON.stringify(data),
+    contentType: "application/json",
+    dataType: 'json',
+    success: function(response)
+    {
+      console.log(response);
+      returnedData = response;
+
+      loadGeneratedNotes(returnedData);
+    }
+  });
+
+
+}
+
+function loadGeneratedNotes(notes)
+{
+  pitch = [];
+  startTimes = [];
+
+  for(var i = 0; i < notes.length; i++)
+  {
+    pitch.push(parseInt(notes[i][0]) - 36);
+  }
+
+  for(var i = 0; i < notes.length; i++)
+  {
+    startTimes.push(parseFloat(notes[i][3]));
+  }
+
+  var clickedNotes = document.querySelectorAll('[data-is-clicked="true"]');
+  var maxColumn = 0;
+
+  console.log(clickedNotes);
+ 
+  for(var i = 0; i < clickedNotes.length; i++)
+  {
+    if(parseInt(clickedNotes[i].id) > maxColumn)
+    {
+      maxColumn = parseInt(clickedNotes[i].id);
+      console.log(maxColumn);
+    }
+  }
+
+  startTime = parseInt(maxColumn);
+  for(var i = 0; i < startTimes.length; i++)
+  {
+    startTimes[i] += startTime;
+  }
+
+  console.log(startTimes);
+
+  var noteRows = document.querySelectorAll(".row");
+  for(var i = 0; i < pitch.length; i++)
+  {
+    if(pitch[i] < 0)
+    {
+      pitch[i] = 0;
+    }
+    console.log("pitch: ", pitch[i]);
+    console.log(noteRows[parseInt(pitch[i])].children[startTime]);
+
+    var generatedColumn = noteRows[parseInt(pitch[i])].children[startTime];
+    generatedColumn.dataset.isGenerated = "true";
+    generatedColumn.dataset.isClicked = "true";
+    notesClicked.push(generatedColumn);
+    console.log(generatedColumn);
+
+    startTime++;
+
+    addColumns(1);
+  }
+}
+
+var noteRows = document.querySelectorAll(".row");
+var noteColumns = document.querySelectorAll(".column");
+var iteration = 0;
+var litUpColumn = [];
+var playing = false;
+var playTimeout;
+
+function play()
+{
+  var clickedNotes = document.querySelectorAll('[data-is-clicked="true"]');
+  var maxColumn = 0;
+
+  console.log(clickedNotes);
+ 
+  for(var i = 0; i < clickedNotes.length; i++)
+  {
+    if(parseInt(clickedNotes[i].id) > maxColumn)
+    {
+      maxColumn = parseInt(clickedNotes[i].id);
+      console.log(maxColumn);
+    }
+  }
+
+  console.log(maxColumn);
+
+  if(clickedNotes.length == 0) return;
+
+  if(iteration == 0)
+  {
+    playing = true;
+  }
+  
+  if(iteration != 0)
+  {
+    for(var i = 0; i < litUpColumn.length; i++)
+    {
+      litUpColumn[i].setAttribute("data-is-playing", "false");
+    }
+    litUpColumn = [];
+  }
+
+  if(iteration >= maxColumn) 
+  {
+    iteration = 0;
+    litUpColumn = [];
+
+    pianoRoll.style.pointerEvents = "auto";
+    clearButton.style.pointerEvents = "auto";
+    generateButton.style.pointerEvents = "auto";
+
+    playing = false;
+
+    return;
+  }
+
+  
+  for(var i = 0; i < noteRows.length; i++)
+  {
+    noteRows[i].children[iteration].setAttribute("data-is-playing", "true");
+    litUpColumn.push(noteRows[i].children[iteration]);
+  }
+
+  for(var i = 0; i < litUpColumn.length; i++)
+  {
+    if(litUpColumn[i].getAttribute("data-is-clicked") == "true")
+    {
+      var row = parseInt(litUpColumn[i].getAttribute("data-row"));
+
+      const audioObject = document.getElementById(notes[row - 24]);
+      console.log(audioObject);
+      const copy = audioObject.cloneNode(true);
+      copy.classList.add("duplicate");
+      
+      var audioDiv = document.querySelector(".audio-files");
+      audioDiv.appendChild(copy);
+      copy.play();
+      window.setInterval(() => {
+        copy.remove();
+      }, 3000)
+    }
+  }
+  
+  iteration++;
+  playTimeout = window.setTimeout(play, 750);
+}
+
+function addColumns(numColumns)
+{
+  var lastColumn = document.querySelectorAll(".row")[0].children[document.querySelectorAll(".row")[0].children.length - 1].id;
+  var rowNumber = 127;
+
+  var rows = document.querySelectorAll(".row");
+  for(var i = 0; i < numColumns; i++)
+  {
+    lastColumn++;
+    for(var j = 0; j < rows.length; j++)
+    {
+      var column = document.createElement("div");
+      column.classList.add("column");
+      column.id = lastColumn;
+      column.dataset.row = rowNumber;
+      column.dataset.isClicked = "false";
+      column.dataset.isPlaying = "false";
+
+      rowNumber--;
+      
+      rows[j].appendChild(column);
+
+      column.addEventListener("click", noteClicked)
+    }
+
+    rowNumber = 127;
+  }
+}
+
